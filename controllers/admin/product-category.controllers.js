254910c7b4509c7fa5f6242e3c1f708af813554e
Ngotruong <ntruonglovek41@gmail.com>
2026-02-01 14:30:49 +0700

    tinh nang chinh sua danh muc san pham

--- controllers/admin/product-category.controllers.js
+++ controllers/admin/product-category.controllers.js
@@ -1,7 +1,8 @@
 const ProductCategory = require("../../model/product-category.model")
 const systemConfig = require("../../config/system");
 
-const createTreeHelper = require("../../helpers/createTree")
+const createTreeHelper = require("../../helpers/createTree");
+const Product = require("../../model/product_model");
 
 
 // [GET] /admin/products-category
@@ -39,6 +40,9 @@ module.exports.create = async (req, res) => {
 
 // [POST] /admin/products/createPost
 module.exports.createPost = async (req, res) => {
+
+    req.body.status = req.body.status ? "active" : "inactive";
+    
     if (req.body.position == "" || isNaN(req.body.position)) {
         const countProducts = await ProductCategory.countDocuments();
         req.body.position = countProducts + 1;
@@ -50,4 +54,54 @@ module.exports.createPost = async (req, res) => {
     await record.save();
 
     res.redirect(`${systemConfig.prefixAdmin}/product-category`);
+};
+
+// [GET] /admin/products-category/edit/:id
+module.exports.edit = async (req, res) => {
+
+    try {
+
+        const id = req.params.id;
+
+        // console.log(id);
+
+        const data = await ProductCategory.findOne({
+            _id: id,
+            deleted: false
+        });
+
+        const records = await ProductCategory.find({
+            deleted: false
+        });
+
+
+        // console.log(records);
+
+        const newRecords = createTreeHelper.tree(records);
+
+        res.render("admin/pages/products-category/edit", {
+            pageTitle: "Chỉnh sửa danh mục sản phẩm",
+            data: data,
+            records: newRecords
+        });
+
+    } catch (error) {
+        res.redirect(`${systemConfig.prefixAdmin}/product-category`);
+
+
+    }
+};
+
+module.exports.editPatch = async (req, res) => {
+    const id = req.params.id;
+
+    req.body.position = parseInt(req.body.position);
+
+    // console.log(id);
+    // console.log(req.body);
+
+    await ProductCategory.updateOne({ _id: id }, req.body);
+
+    res.redirect(`${systemConfig.prefixAdmin}/product-category`);
+
 };
--- routes/admin/product-category.route.js
+++ routes/admin/product-category.route.js
@@ -17,5 +17,17 @@ router.post(
     controller.createPost
 );
 
+router.get("/edit/:id", controller.edit);
+
+router.patch(
+    "/edit/:id",
+    upload.single("thumbnail"),
+    validate.createPost,
+    uploadCloud.upload,
+    controller.editPatch,
+
+);
+
+
 
 module.exports = router;
